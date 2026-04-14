f82e09d75aeee8ee269bfd1ffe702baea92cc43b
Guillaume <gsagbo541@gmail.com>
2026-04-14 17:15:19 +0100

    config deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/themeContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
